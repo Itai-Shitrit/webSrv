5234c32ef2b6591173f2d7cd405e046d9ab29906
Itai <itai0528828299@gmail.com>
2026-07-20 22:29:35 +0300

    orginal

--- app.js
+++ app.js
@@ -53,10 +53,4 @@ app.use(express.static('public'));
 app.use('/product',routerProduct); // שילוב של הראוטר מוצרים 
 app.use('/user',routerUser); // שילוב של הראוטר מוצרים 
 
-const PORT = process.env.PORT || 5050;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-app.listen(PORT, '0.0.0.0', () => {});
-
 module.exports=app; // יצא של המודול שכתבנו
